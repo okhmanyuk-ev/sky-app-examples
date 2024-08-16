47fdab674ec58de45f779d0fdda0a69d504ef71f
Evgeniy Okhmanyuk <3295141+okhmanyuk-ev@users.noreply.github.com>
2024-08-16 20:40:13 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,8 +1,8 @@
 function Frame()
-	Gfx.Clear(0.125, 0.125, 0.125, 1.0);
-	Gfx.Begin(Gfx.Triangles);
-	Gfx.Vertex(0.5, -0.5, 0.0, 0.0, 0.0, 1.0, 1.0);
-	Gfx.Vertex(-0.5, -0.5, 0.0, 1.0, 0.0, 0.0, 1.0);
-	Gfx.Vertex(0.0, 0.5, 0.0, 0.0, 1.0, 0.0, 1.0);
-	Gfx.End();
+	Gfx.Clear(0.125, 0.125, 0.125, 1.0)
+	Gfx.Begin(Gfx.Triangles)
+	Gfx.Vertex(0.5, -0.5, 0.0, 0.0, 0.0, 1.0, 1.0)
+	Gfx.Vertex(-0.5, -0.5, 0.0, 1.0, 0.0, 0.0, 1.0)
+	Gfx.Vertex(0.0, 0.5, 0.0, 0.0, 1.0, 0.0, 1.0)
+	Gfx.End()
 end
