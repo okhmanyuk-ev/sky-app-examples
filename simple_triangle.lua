d2526723ce81e9a6151dfa631b90d5806bc0ac41
Evgeniy Okhmanyuk <3295141+okhmanyuk-ev@users.noreply.github.com>
2024-08-22 22:54:02 +0300

    Update simple_triangle.lua

--- simple_triangle.lua
+++ simple_triangle.lua
@@ -1,5 +1,5 @@
 function Frame()
-	Gfx.Clear(0.125, 0.125, 0.125, 1.0)
+	Gfx.Clear(0.25, 0.0, 0.25, 1.0);
 	Gfx.Begin(Gfx.Mode.Triangles)
 	Gfx.Vertex(0.5, -0.5, 0.0, 0.0, 0.0, 1.0, 1.0)
 	Gfx.Vertex(-0.5, -0.5, 0.0, 1.0, 0.0, 0.0, 1.0)
