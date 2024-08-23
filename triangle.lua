7ea55155632a1217e396d446708a518cde44d423
Evgeniy Okhmanyuk <3295141+okhmanyuk-ev@users.noreply.github.com>
2024-08-23 20:52:20 +0300

    Update triangle.lua

--- triangle.lua
+++ triangle.lua
@@ -1,8 +1,8 @@
 function Frame()
-	Gfx.Clear(0.125, 0.125, 0.125, 1.0)
+	Gfx.Clear(0, 0.25, 0.125, 1.0)
 	Gfx.Begin(Gfx.Mode.Triangles)
-	Gfx.Vertex(Vertex():WithPos(Vec3(0.5, -0.5, 0.0)):WithColor(Vec4(1, 0, 0, 1)))
-	Gfx.Vertex(Vertex():WithPos(Vec3(-0.5, -0.5, 0.0)):WithColor(Vec4(0, 1, 0, 1)))
-	Gfx.Vertex(Vertex():WithPos(Vec3(0.0, 0.5, 0.0)):WithColor(Vec4(0, 0, 1, 1)))
+	Gfx.Vertex(Vertex():WithPos(Vec3(0.5, -0.5, 0)):WithColor(Vec4(1, 0, 0, 1)))
+	Gfx.Vertex(Vertex():WithPos(Vec3(-0.5, -0.5, 0)):WithColor(Vec4(0, 1, 0, 1)))
+	Gfx.Vertex(Vertex():WithPos(Vec3(0.0, 0.5, 0)):WithColor(Vec4(0, 0, 1, 1)))
 	Gfx.End()
 end
