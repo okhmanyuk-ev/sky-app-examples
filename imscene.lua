dc098f638ef2f16b5efb2208673f60c6b8a14da8
Evgeniy Okhmanyuk <3295141+okhmanyuk-ev@users.noreply.github.com>
2024-08-28 19:51:31 +0300

    Update imscene.lua

--- imscene.lua
+++ imscene.lua
@@ -7,6 +7,10 @@ function Frame()
 		bg.Touchable = true
 	end
 
+	if ImScene.IsMouseHovered(bg) then
+		ImScene.Tooltip(bg, "background")
+	end
+
 	local green = ImScene.SpawnRectangle(bg)
 	if ImScene.IsFirstCall() then
 		green.Anchor = Vec2(0.5, 0.5)
@@ -14,11 +18,25 @@ function Frame()
 		green.Size = Vec2(128, 128)
 		green.Color = Vec4(0.5, 0.75, 0.5, 1)
 		green.Touchable = true
+
+		local label = Scene.Label.Create()
+		label.Anchor = Vec2(0.5, 0.0)
+		label.Pivot = Vec2(0.5, 1.0)
+		label.Y = -64
+		label.Text = "move mouse inside green rectangle"
+		label.FontSize = 32
+		green:Attach(label)
 	end
 
-	if ImScene.IsMouseHovered(bg) then
-		ImScene.Tooltip(bg, "background")
-	elseif ImScene.IsMouseHovered(green) then
+	if ImScene.IsMouseHovered(green) then
 		ImScene.Tooltip(bg, "green rectangle")
+
+		local blue = ImScene.SpawnRectangle(bg)
+		if ImScene.IsFirstCall() then
+			blue.Anchor = Vec2(0.75, 0.5)
+			blue.Pivot = Vec2(0.5, 0.5)
+			blue.Size = Vec2(128, 128)
+			blue.Color = Vec4(0.5, 0.5, 0.75, 1)
+		end
 	end
 end
