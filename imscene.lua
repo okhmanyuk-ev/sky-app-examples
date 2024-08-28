5b2366c5a42c154ead722cf7f976283e400ccc53
Evgeniy Okhmanyuk <3295141+okhmanyuk-ev@users.noreply.github.com>
2024-08-28 19:31:59 +0300

    Update imscene.lua

--- imscene.lua
+++ imscene.lua
@@ -16,31 +16,9 @@ function Frame()
 		green.Touchable = true
 	end
 
-	local red = ImScene.SpawnRectangle(bg)
-	if ImScene.IsFirstCall() then
-		red.Anchor = Vec2(0.25, 0.5)
-		red.Pivot = Vec2(0.5, 0.5)
-		red.Size = Vec2(128, 128)
-		red.Color = Vec4(0.75, 0.5, 0.5, 1)
-		red.Touchable = true
-	end
-
-	local blue = ImScene.SpawnRectangle(bg)
-	if ImScene.IsFirstCall() then
-		blue.Anchor = Vec2(0.75, 0.5)
-		blue.Pivot = Vec2(0.5, 0.5)
-		blue.Size = Vec2(128, 128)
-		blue.Color = Vec4(0.5, 0.5, 0.75, 1)
-		blue.Touchable = true
-	end
-
 	if ImScene.IsMouseHovered(bg) then
 		ImScene.Tooltip(bg, "background")
-	elseif ImScene.IsMouseHovered(red) then
-		ImScene.Tooltip(bg, "red rectangle")
 	elseif ImScene.IsMouseHovered(green) then
 		ImScene.Tooltip(bg, "green rectangle")
-	elseif ImScene.IsMouseHovered(blue) then
-		ImScene.Tooltip(bg, "blue rectangle")
 	end
 end
