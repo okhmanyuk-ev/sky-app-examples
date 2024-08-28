e5ce794ee05aafd25ae84198421e625a34c1f55d
Evgeniy Okhmanyuk <3295141+okhmanyuk-ev@users.noreply.github.com>
2024-08-28 19:54:29 +0300

    Update sprites.lua

--- sprites.lua
+++ sprites.lua
@@ -2,7 +2,7 @@ local bg = Scene.Rectangle.Create()
 bg.Stretch = Vec2(1, 1)
 bg.TopColor = Vec4(0, 0.25, 0, 1)
 bg.BottomColor = Vec4(0, 0.125, 0, 1)
-Scene.GetRoot():Attach(bg)
+Scene.Root:Attach(bg)
 
 local simple = Scene.Sprite.Create()
 simple.Anchor = Vec2(0.5, 0.5)
